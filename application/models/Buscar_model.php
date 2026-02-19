6896c720f193b021477006c9d9eb5042f943974b
Rafael San José <rsanjose@alxarafe.com>
2026-02-19 20:35:37 +0100

    Standardize Alxarafe copyright headers across the project

--- application/models/Buscar_model.php
+++ application/models/Buscar_model.php
@@ -1,13 +1,22 @@
 <?php
 
 /**
- * rSanjoSEO.
+ * Alxarafe WorkFrame.
  *
- * Copyright (c)2014-2026, Rafael San José Tovar
+ * Copyright (C) 2018 - 2026 Rafael San José Tovar <rsanjose@alxarafe.com>
  *
- * @author		rSanjoSEO (rsanjose@alxarafe.com)
- * @copyright	Copyright (c)2018, Rafael San José Tovar (https://alxarafe.es/)
- * @license		Prohibida su distribución total o parcial. Uso sujeto a contrato comercial.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 defined('BASEPATH') or exit('No direct script access allowed');
 
